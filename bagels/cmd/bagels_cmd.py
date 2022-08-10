98a322e96aec4f54136376c01e0eeffef079a070
rexfelix <rexfelix@naver.com>
2022-08-11 04:08:06 +0900

    Bagels package

--- bagels/cmd/bagels_cmd.py
+++ bagels/cmd/bagels_cmd.py
@@ -3,11 +3,8 @@ from bagels.bagels import bagels
 
 
 def bagels_play():
-    parser = argparse.ArgumentParser(
+    argparse.ArgumentParser(
         description="세 자리 수 맞추기 게임"
     )
 
-    parser.add_argument("--limit", action="store", type=int, required=False,
-                        help="추측 횟수 제한 하기")
-    args = parser.parse_args()
-    bagels(_limit=args.limit)
+    bagels()
