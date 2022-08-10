b66dd975b4c2efffebcdf0520edc80f3acabaa0e
rexfelix <rexfelix@naver.com>
2022-08-11 02:46:44 +0900

    Bagels package

--- setuo.py
+++ setuo.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fn:
     long_description = fn.read()
@@ -9,8 +9,13 @@ setup(
     author="Rexfelix",
     author_email="rexfelix@naver.com",
     description="세 자리 숫자 맞추기 게임",
-    long_description="세 자리 숫자를 맞추는 게임 입니다.\n추측한 숫자가 맞지만 위치가 틀리면 'Pico',\n숫자도 맞고 자리도 맞으면 'Fermi',\n맞는 숫자가 없으면 'Bagel' "
-                     "힌트를 제공 합니다. ",
+    long_description=long_description,
     url="https://github.com/rexfelix/python_example",
     include_dirs=[],
+    packages=find_packages(exclude=("tests",)),
+    classifiers=[
+        "Development Status::4-Beta",
+        "Programming Language::Python::3",
+        "Operating System::OS Independent",
+    ],
 )
